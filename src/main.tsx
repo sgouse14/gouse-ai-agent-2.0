31cf5b2aa3aeebfb53a8f38e5af37092deb95e80
sgouse14 <127516656+sgouse14@users.noreply.github.com>
2026-09-22 17:01:54 +0530

    feat(saqlain): expose security workspace route

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { SaqlainSecurityView } from './components/SaqlainSecurityView';
 import { ThemeProvider } from './context/ThemeContext';
 import './index.css';
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
+  const securityMode = window.location.hash === '#saqlain';
   ReactDOM.createRoot(rootElement).render(
     <React.StrictMode>
       <ThemeProvider>
-        <App />
+        {securityMode ? <SaqlainSecurityView /> : <App />}
       </ThemeProvider>
     </React.StrictMode>
   );
 }
-
